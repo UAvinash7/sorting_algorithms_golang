26853f76bc78aec48104c3813320035af9e28a90
avinash upadhyayula <avinash.upadhyayula@gmail.com>
2025-02-28 22:16:34 +0530

    sorting algorithms

---
+++ 4_quick_sort/quick_sort.go
@@ -0,0 +1,9 @@
+/*
+
+1. It uses divide and conquer.
+2. A pivot item is choosen, determining its final position in the array.
+3. After that smaller items are on the left of the pivot and larger are on the right.
+4. The left and right sub-arrays are processed recursively in the same way.
+
+*/
+
