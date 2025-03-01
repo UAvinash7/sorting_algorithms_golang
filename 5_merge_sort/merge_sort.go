3668531192de2c9ba947e240a6a3d04bbacbf04b
avinash upadhyayula <avinash.upadhyayula@gmail.com>
2025-03-01 22:14:12 +0530

    sorting algorithms

---
+++ 5_merge_sort/merge_sort.go
@@ -0,0 +1,9 @@
+/*
+
+1. It uses divide and conquer approach.
+2. The array is divided into two equal halves. Each half is recursively processed.
+3. At the end off each recursive step, there are two sorted sub-arrays.
+4. These two sorted sub-arrays are merged into a sorted one.
+
+*/
+
