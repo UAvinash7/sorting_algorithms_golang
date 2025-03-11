00294eff19c1d91daea1e65abc941024d2f0ec40
avinash upadhyayula <avinash.upadhyayula@gmail.com>
2025-03-11 21:57:06 +0530

    sorting algorithms

---
+++ 7_counting_sort/counting_sort.go
@@ -0,0 +1,8 @@
+/*
+
+1. It iterates through the array to find the number of elements.
+2. It creates a counter array of size (number of elements + 1) with all zero's.
+3. It iterates the array and updates the counter array accordingly.
+4. It sorts the array using the counter array.
+
+*/
---
+++ 8_radix_sort/radix_sort.go
@@ -0,0 +1,8 @@
+/*
+
+1. It finds the maximum value in the array, determining how many digits it has.
+2. It iterates over each element in the array considering first unit's place, then ten's place, then hundred's place etc... etc...
+3. It arranges the elements based on sorted numbers in that digit's place.
+
+*/
+
