0079690bbc1cae2f80dafe6de0dfd1b274e6a48d
avinash upadhyayula <avinash.upadhyayula@gmail.com>
2025-03-13 22:59:13 +0530

    sorting algorithms

--- 4_quick_sort/quick_sort.go
+++ 4_quick_sort/quick_sort.go
@@ -4,6 +4,8 @@
 2. A pivot item is choosen, determining its final position in the array.
 3. After that smaller items are on the left of the pivot and larger are on the right.
 4. The left and right sub-arrays are processed recursively in the same way.
+Time Complexity- O(n log (n)) in best and average case and O(n ^ 2) in worst case.
+Space Complexity- O(n)
 
 */
 
@@ -11,17 +13,45 @@ package main
 
 import "fmt"
 
-func quickSort(inputArray []int) {}
+func quickSort(inputArray []int, lbound, ubound int) []int {
+	if lbound < ubound {
+		inputArray, location := partition(inputArray, lbound, ubound)
+		quickSort(inputArray, lbound, location-1)
+		quickSort(inputArray, location+1, ubound)
+	}
+	return inputArray
+}
+
+func partition(inputArr []int, lowerBound, upperBound int) ([]int, int) {
+	pivotElement := inputArr[lowerBound]
+	start := lowerBound
+	end := upperBound
+	for start < end {
+		for inputArr[start] <= pivotElement {
+			start++
+		}
+		for inputArr[end] > pivotElement {
+			end--
+		}
+		if start < end {
+			inputArr[start], inputArr[end] = inputArr[end], inputArr[start]
+		}
+	}
+	inputArr[lowerBound], inputArr[end] = inputArr[end], inputArr[lowerBound]
+	return inputArr, end
+}
 
 func main() {
 	var numberOfElements, temporaryVariable int
 	fmt.Println("Enter the value of number of elements:")
 	fmt.Scanf("%d\n", &numberOfElements)
-	var arr = make([]int, numberOfElements)
+	var userInputArray = make([]int, numberOfElements)
 	for i := 0; i < numberOfElements; i++ {
 		fmt.Printf("Enter the value of %d element:\n", i)
 		fmt.Scanf("%d\n", &temporaryVariable)
-		arr[i] = temporaryVariable
+		userInputArray[i] = temporaryVariable
 	}
-	fmt.Println("arr:", arr)
+	fmt.Println("user input array:", userInputArray)
+	lb, ub := userInputArray[0], numberOfElements-1
+	fmt.Println("Sorted array using quick sort mechanism:", quickSort(userInputArray, lb, ub))
 }
