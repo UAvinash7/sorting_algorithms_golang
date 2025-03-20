5f8307493092bc393e98f33bc0631da3cfddf25c
avinash upadhyayula <avinash.upadhyayula@gmail.com>
2025-03-20 22:16:45 +0530

    sorting algorithms

--- 10_bucket_sort/bucket_sort.go
+++ 10_bucket_sort/bucket_sort.go
@@ -1,3 +1,19 @@
+/*
+
+Bucket Sort or Bin Sort works by distributing the elements of an array into a number of buckets.
+
+Each bucket is then sorted individually, either using a different sorting algorithm or by recursively applying the bucket sorting algorithm.
+
+It works as-
+
+1. Setup an array of initially empty "buckets".
+2. Scatter: Go over the original array, putting each element in its bucket.
+3. Sort each non-empty bucket.
+4. Gather: Visit the buckets, in order and put all elements back into the original array.
+
+*/
+
+
 package main
 
 func main() {
