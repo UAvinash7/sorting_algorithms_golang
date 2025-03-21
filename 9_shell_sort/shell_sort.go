a259964dee7f448ddd1ff19e30a69d317633f6f9
avinash upadhyayula <avinash.upadhyayula@gmail.com>
2025-03-21 22:42:56 +0530

    sorting algorithms

--- 9_shell_sort/shell_sort.go
+++ 9_shell_sort/shell_sort.go
@@ -1,3 +1,11 @@
+/*
+
+Shell Sort is a generalized version of Insertion Sort algorithm.
+It first sorts elements that are far apart from each other and successfully reduces the interval between the elements to be sorted.
+
+*/
+
+
 package main
 
 func shellSort(){
@@ -5,5 +13,5 @@ func shellSort(){
 }
 
 func main() {
-	
+
 }
